879c7276427e4c467246926463cbf7ae7d9acbdc
Jaye <jaye1248276@gmail.com>
2022-07-19 15:14:28 +0800

    [add]: 新增useDownload hooks

---
+++ src/hooks/web/useDownload.js
@@ -0,0 +1,16 @@
+/**
+ * @Date 2022-07-15 09:53:57
+ * @description 自动下载
+ * @param { Blob } data
+ * @param { String } filename
+ * @return void
+**/
+export default function useDownload(data, filename) {
+  const url = URL.createObjectURL(data)
+  const link = document.createElement('a')
+  link.href = url
+  link.download = filename
+  link.click()
+  link.remove()
+  URL.revokeObjectURL(url)
+}
