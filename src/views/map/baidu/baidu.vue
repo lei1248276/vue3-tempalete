83c72801c8c82570ed964c70f283655db3e43944
jaye <jaye1248276@gmail.com>
2023-05-29 16:25:19 +0800

    [fix]: 修复百度地图页面刷新无效问题

--- src/views/map/baidu/baidu.vue
+++ src/views/map/baidu/baidu.vue
@@ -14,23 +14,31 @@ defineOptions({
 (window as any).BMap_loadScriptTime = new Date().getTime()
 const BAI_DU_MAP_URL = 'https://api.map.baidu.com/getscript?v=3.0&ak=uiEul1IBO29C6OEwDAunXXvEs0pIRNV9&services=&t=20230512140248'
 
-useScriptTag(
+const { unload } = useScriptTag(
   BAI_DU_MAP_URL,
   () => {
-    nextTick(() => {
-      // 百度地图API功能
-      const BMap = (window as any).BMap
-      const map = new BMap.Map('baiduRef') // 创建Map实例
-      const point = new BMap.Point(116.331398, 39.897445)
-      map.centerAndZoom(point, 12) // 初始化地图,用城市名设置地图中心点
+    // 百度地图API功能
+    const BMap = (window as any).BMap
+    const map = new BMap.Map('baiduRef') // 创建Map实例
+    const point = new BMap.Point(116.331398, 39.897445)
+    map.centerAndZoom(point, 12) // 初始化地图,用城市名设置地图中心点
+    map.addControl(new BMap.NavigationControl())
+    map.addControl(new BMap.ScaleControl())
+    map.addControl(new BMap.OverviewMapControl())
+    map.addControl(new BMap.MapTypeControl())
 
-      function myFun(result: any) {
-        const cityName = result.name
-        map.setCenter(cityName)
-      }
-      const myCity = new BMap.LocalCity()
-      myCity.get(myFun)
-    })
+    function myFun(result: any) {
+      const cityName = result.name
+      map.setCenter(cityName)
+    }
+    const myCity = new BMap.LocalCity()
+    myCity.get(myFun)
   }
 )
+
+const tagsViewStore = useTagsViewStore()
+onDeactivated(() => {
+  // ! 在组件刷新的时候手动卸载‘<script>’标签
+  !tagsViewStore.cachedViews.includes('Baidu') && unload()
+})
 </script>
